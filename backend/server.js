6bed2822d2b6e2c46f332325afaa935e48a738d1
kazu2377 <jtths474@hotmail.co.jp>
2025-08-12 16:22:40 +0900

    Expressアプリケーションにミドルウェアとトランザクションエンドポイントを追加

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // パッケージのインポート
 import dotenv from "dotenv";
 import express from "express";
-import { initDB } from "./config/db.js";
+import { initDB,sql } from "./config/db.js";
 
 // 環境変数の読み込み
 dotenv.config();
@@ -9,6 +9,12 @@ dotenv.config();
 // Expressアプリケーションのインスタンスを作成
 const app = express();
 const PORT = process.env.PORT || 5001; // ポート番号を環境変数から取得、なければ5001を使用
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("ミドルウェア", req.method);
+  next();
+});
 
 // ルートエンドポイントの作成
 app.get("/", (req, res) => {
@@ -31,8 +37,30 @@ console.log(process.env.PORT);
 //   console.log(`Server is up and running on PORT:${PORT}`);
 // });
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is up and running on PORT:", PORT);
   });
+});
+
+app.post("/api/transactions", async (req, res) => {
+  try {
+
+    const { title, amount, category, user_id } = req.body;
+    console.log("Received transaction data:", req.body);
+
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // ここにデータベースへの挿入ロジックを追加します
+    const transaction = await sql`INSERT INTO transactions (user_id, title, category, amount) VALUES (${user_id}, ${title}, ${category}, ${amount}) RETURNING *`;
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.error("Error adding transaction:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
