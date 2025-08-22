f7126f53fb20ba56e26a694fbdab3f15eaae0fd2
kazu2377 <jtths474@hotmail.co.jp>
2025-08-23 07:16:02 +0900

    feat(mobile)：ExpoとTypeScriptの設定でモバイルアプリを初期化する。
    - Expo、React Navigation、その他のライブラリの依存関係をpackage.jsonに追加。
    - 異なるプラットフォームでアプリを起動するためのスクリプトを追加。
    - 厳密な型チェックとパスエイリアスでExpoの基本設定を拡張するtsconfig.jsonを作成。

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,14 @@
+import SafeScreen from "@/components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+    </ClerkProvider>
+  );
+}
---
+++ mobile/app/about.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const About = () => {
+  return (
+    <View>
+      <Text>About this app</Text>
+    </View>
+  );
+};
+
+export default About;
